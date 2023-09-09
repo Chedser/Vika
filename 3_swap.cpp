762f3d126122f5524adb51f5560f1c9a38904f73
Vectorflex <chedser@gmail.com>
2023-09-09 14:58:55 +0300

    Create 3_swap.cpp

---
+++ 3_swap.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+/* Перестановка элементов */
+template<typename T>void Swap(T* first, T* second, T* third) {
+    array<T, 3> arr = { *first, *second, *third };
+    std::sort(arr.begin(), arr.end());
+    *first = arr[0];
+    *second = arr[1];
+    *third = arr[2];
+}
+
+int main(){
+    setlocale(LC_ALL, "ru");
+    int first = 3;
+    int second = 2;
+    int third = 1;
+
+    cout << "Начальные элементы:" << endl;
+    cout << "Первый: " << first << endl;
+    cout << "Второй: " << second << endl;
+    cout << "Третий: " << third << endl;
+
+    cout << endl;
+    
+    Swap(&first, &second, &third);
+
+    cout << "Конечные элементы:" << endl;
+    cout << "Первый: " << first << endl;
+    cout << "Второй: " << second << endl;
+    cout << "Третий: " << third << endl;
+    
+    return 0;
+}
