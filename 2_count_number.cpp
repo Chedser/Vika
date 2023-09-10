c908cdb61f42b03b540959ba8fff132a7440493f
Vectorflex <chedser@gmail.com>
2023-09-10 06:37:27 +0300

    Update 2_count_number.cpp

--- 2_count_number.cpp
+++ 2_count_number.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 // Функция генерирования случайного целочисленного числа в указанных пределах.
 // Диапазон чисел: [min, max]
-int GetRandomNumber(int min, int max){
+int GetRandomNumber(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
-template<typename T> int CountOfNumber(T* x, int i, T numberToFind){
+template<typename T> int CountOfNumber(T* x, int i, T numberToFind) {
     if (i < 0) return 0; // выход из рекурсии, когда проверены все элементы
     int n = 0;
     if (x[i] > numberToFind) n = 1;  // если входит в интервал, то засчитываем
     return n + CountOfNumber(x, i - 1, numberToFind); // суммируем засчитанные элементы
 }
 
-int main(){
+int main() {
     setlocale(LC_ALL, "ru");
 
     const int COUNT = 20;
     int arr[COUNT];
     int numberToFind;
-    
+
     srand(time(NULL));
     /* Заполнение массива случайными числами */
     for (int i = 0; i < COUNT; i++) {
@@ -36,14 +36,15 @@ int main(){
     }
 
     cout << endl;
-    cout << "Введите число, количество которое нужно подсчитать:" << endl;
+    cout << "Введите число:" << endl;
 
     cin >> numberToFind;
     cout << endl;
 
     if (cin.good()) {
-        cout << "Количество " << numberToFind << ": " << CountOfNumber(arr, COUNT - 1, numberToFind) << endl;
-    }else {
+        cout << "Количество чисел больших " << numberToFind << ": " << CountOfNumber(arr, COUNT - 1, numberToFind) << endl;
+    }
+    else {
         cout << "Ошибка ввода";
     }
 
